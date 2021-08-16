ba2a2682cbd5c0b404fb6b78f94f6224556a1405
cqrlosArch <diaz.flores.c@gmail.com>
2021-08-16 20:15:50 +0200

    urlHeroku

--- client/src/api/connect.js
+++ client/src/api/connect.js
@@ -1,18 +1,20 @@
 import axios from 'axios';
 
+const urlDatabase = 'https://api-unsplash-v2.herokuapp.com/api/images';
+
 const getImagesDB = async () => {
-  const res = await axios.get('http://localhost:8080/api/images');
+  const res = await axios.get(urlDatabase);
   return res.data;
 };
 const removeImageDB = async (id) => {
-  await axios.delete(`http://localhost:8080/api/images/${id}`);
+  await axios.delete(`${urlDatabase}/${id}`);
 };
 
 const addImageDB = async (image) => {
   const options = {
     method: 'POST',
     data: image,
-    url: `http://localhost:8080/api/images/`,
+    url: urlDatabase,
   };
   await axios(options);
 };
